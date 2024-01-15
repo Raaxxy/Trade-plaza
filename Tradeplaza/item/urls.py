eb43fe280d44262e0910d3abc10db316a03e61cf
Raaxxy <rahularyan0197@gmail.com>
2024-01-15 05:35:32 +0530

    added dashboard

--- Tradeplaza/item/urls.py
+++ Tradeplaza/item/urls.py
@@ -7,4 +7,8 @@ app_name = 'item'
 urlpatterns = [
     path('new/',views.new,name='new'),
     path('<int:pk>/',views.detail,name='detail'),
+    path('<int:pk>/delete/',views.delete,name='delete'),
+    path('<int:pk>/delete_confirmation/', views.delete_confirmation, name='delete_confirmation'),
+    path('cancel_delete/', views.cancel_delete, name='cancel_delete'),
+    path('<int:pk>/edit/',views.edit,name='edit'),
 ] 
