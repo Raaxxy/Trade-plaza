fa4e99083f601b73044a6444a182f551842fe8c0
Raaxxy <rahularyan0197@gmail.com>
2024-01-16 18:14:15 +0530

    Added search item

--- Tradeplaza/item/forms.py
+++ Tradeplaza/item/forms.py
@@ -30,5 +30,34 @@ class NewItemForm(forms.ModelForm):
         'class': 'bg-slate-300 border border-gray-300 text-gray-900 placeholder:text-gray-600 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5'
     }))
 
+class EditItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('name','description','price','image','is_sold')
+    name = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Type product name',
+        'class': 'bg-slate-300 border border-gray-300 text-gray-900 placeholder:text-gray-600 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5'
+    }))
+
+    description = forms.CharField(widget=forms.Textarea(attrs={
+        'placeholder': 'Write product description here',
+        'class': 'block p-2.5 w-full text-sm text-gray-900 placeholder:text-gray-600 bg-slate-300 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500'
+    }))
+
+    price = forms.DecimalField(widget=forms.NumberInput(attrs={
+        'placeholder': 'eg.$49.99',
+        'class': 'bg-slate-300 border border-gray-300 text-gray-900 placeholder:text-gray-600 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5'
+    }))
+
+    image = forms.ImageField(widget=forms.ClearableFileInput(attrs={
+        'class': 'bg-slate-300 border border-gray-300 text-gray-900 placeholder:text-gray-600 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5'
+    }))
     
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.fields['image'].widget.initial_text = 'Current Image'
+        self.fields['image'].widget.input_text = 'Upload the new Image'
+
+    is_sold = forms.BooleanField(required=False,widget=forms.CheckboxInput())
        
--- Tradeplaza/item/views.py
+++ Tradeplaza/item/views.py
@@ -1,9 +1,36 @@
 from django.contrib.auth.decorators import login_required
-
+from django.db.models import Q
 from django.shortcuts import render,get_object_or_404,redirect
 
-from .forms import NewItemForm
-from .models import Item
+from .forms import NewItemForm, EditItemForm
+from .models import Category,Item
+
+
+def items(request):
+    query = request.GET.get('query','')
+    category_id = request.GET.get('category',0)
+    categories = Category.objects.all()
+    items = Item.objects.filter(is_sold=False)
+
+    # potential search histoy option
+    #search_history=request.session.get('search_history', [])  
+    # if query and query not in search_history:
+    #     search_history.append(query)    
+    # max_history_length = 10
+    # if len(search_history) > max_history_length:
+    #     search_history = search_history[-max_history_length:]
+    if category_id:
+        items = items.filter(category_id=category_id)
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    
+    return render(request, 'item/items.html',{
+        'items':items,
+        'query':query,
+        'categories':categories,
+        'category_id':int(category_id),
+      #  'search_history': search_history,
+    })
 
 def detail(request,pk):
     item = get_object_or_404(Item,pk=pk)
@@ -30,4 +57,38 @@ def new(request):
         'form' : form,
         'title' : 'List Item',
     })
+
+@login_required
+def edit(request,pk):
+    item = get_object_or_404(Item,pk=pk,created_by=request.user)
+
+    if request.method == 'POST':
+        form = EditItemForm(request.POST,request.FILES, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('item:detail',pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+    
+
+    return render(request, 'item/editform.html',{
+        'form' : form,
+        'title' : 'Edit Item',
+    })
+
+
+@login_required
+def delete(request,pk):
+    item = get_object_or_404(Item,pk=pk,created_by=request.user)
+    item.delete()
+    return redirect('dashboard:index')
+
+def delete_confirmation(request,pk):
+    item = get_object_or_404(Item,pk=pk,created_by=request.user)
+    return render(request, 'item/confirmation.html', {
+        'item': item
+    })
+
+def cancel_delete(request):
+    return redirect('dashboard:index')
 
