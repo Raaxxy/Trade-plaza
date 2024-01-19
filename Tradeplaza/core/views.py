0a6adddcc2f943152d3cbe414d0e0a4635a537fe
Raaxxy <rahularyan0197@gmail.com>
2024-01-19 10:04:13 +0530

    added dropdown menu for screens below md

--- Tradeplaza/core/views.py
+++ Tradeplaza/core/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import logout
-
 from item.models import Category,Item
-
 from .forms import SignupForm
 
 def index (request):
