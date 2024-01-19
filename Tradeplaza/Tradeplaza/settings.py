b63c2e76887b9f4ea1c88e38d0ac767b365ee9cc
Raaxxy <rahularyan0197@gmail.com>
2024-01-19 20:56:03 +0530

    db migrate to postgresql

--- Tradeplaza/Tradeplaza/settings.py
+++ Tradeplaza/Tradeplaza/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'Tradeplaza.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tradeplaza',
+        'USER': 'raxxy',
+        'PASSWORD': 'codemarly',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
