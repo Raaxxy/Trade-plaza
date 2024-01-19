72ae596976ab70a1c609e728d6362b4721147a81
Raaxxy <rahularyan0197@gmail.com>
2024-01-19 21:07:29 +0530

    changed requirements.txt

--- Tradeplaza/Tradeplaza/settings.py
+++ Tradeplaza/Tradeplaza/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kwj5z7#mg$9th)*ld(5#x%7h91wlsn)#xrn!(q!7bsgjbs3*ld'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app']
 
