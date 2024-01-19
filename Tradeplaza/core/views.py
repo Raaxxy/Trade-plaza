2dcc0a7ddf2e2b93f3fcc91dff28c2633b0802b4
Raaxxy <rahularyan0197@gmail.com>
2024-01-19 07:38:03 +0530

    added about views

--- Tradeplaza/core/views.py
+++ Tradeplaza/core/views.py
@@ -13,8 +13,8 @@ def index (request):
         'items': items,
     })
 
-def contact (request):
-    return render(request, 'core/contact.html')
+def about (request):
+    return render(request, 'core/about.html')
 
 def signup(request):
     if request.method == 'POST':
