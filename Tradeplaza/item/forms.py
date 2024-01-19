d062ad2b748d4710df8e551fb9ac8529baa3ab1e
Raaxxy <rahularyan0197@gmail.com>
2024-01-19 07:38:35 +0530

    added responsiveness to forms

--- Tradeplaza/item/forms.py
+++ Tradeplaza/item/forms.py
@@ -8,26 +8,26 @@ class NewItemForm(forms.ModelForm):
 
 
     category = forms.ModelChoiceField(queryset=Category.objects.all(),widget=forms.Select(attrs={
-        'class': 'bg-slate-300 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5'
+        'class': 'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
     }))
        
     name = forms.CharField(widget=forms.TextInput(attrs={
         'placeholder': 'Type product name',
-        'class': 'bg-slate-300 border border-gray-300 text-gray-900 placeholder:text-gray-600 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5'
+        'class': 'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
     }))
 
     description = forms.CharField(widget=forms.Textarea(attrs={
         'placeholder': 'Write product description here',
-        'class': 'block p-2.5 w-full text-sm text-gray-900 placeholder:text-gray-600 bg-slate-300 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500'
+        'class': 'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
     }))
 
     price = forms.DecimalField(widget=forms.NumberInput(attrs={
         'placeholder': 'eg.$49.99',
-        'class': 'bg-slate-300 border border-gray-300 text-gray-900 placeholder:text-gray-600 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5'
+        'class': 'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
     }))
 
     image = forms.ImageField(widget=forms.ClearableFileInput(attrs={
-        'class': 'bg-slate-300 border border-gray-300 text-gray-900 placeholder:text-gray-600 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5'
+        'class': 'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
     }))
 
 class EditItemForm(forms.ModelForm):
@@ -36,28 +36,38 @@ class EditItemForm(forms.ModelForm):
         fields = ('name','description','price','image','is_sold')
     name = forms.CharField(widget=forms.TextInput(attrs={
         'placeholder': 'Type product name',
-        'class': 'bg-slate-300 border border-gray-300 text-gray-900 placeholder:text-gray-600 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5'
+        'class': 'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+
+        
     }))
 
     description = forms.CharField(widget=forms.Textarea(attrs={
         'placeholder': 'Write product description here',
-        'class': 'block p-2.5 w-full text-sm text-gray-900 placeholder:text-gray-600 bg-slate-300 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500'
+        'class': 'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+
+        
     }))
 
     price = forms.DecimalField(widget=forms.NumberInput(attrs={
         'placeholder': 'eg.$49.99',
-        'class': 'bg-slate-300 border border-gray-300 text-gray-900 placeholder:text-gray-600 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5'
+        'class': 'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+
+        
     }))
 
     image = forms.ImageField(widget=forms.ClearableFileInput(attrs={
-        'class': 'bg-slate-300 border border-gray-300 text-gray-900 placeholder:text-gray-600 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5'
+         'class': 'bg-gray-50 border border-gray-300 text-white sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 placeholder:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+         #this part needs a little bit of polishing
     }))
     
+    is_sold = forms.BooleanField(required=False,widget=forms.CheckboxInput())
+
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
         self.fields['image'].widget.initial_text = 'Current Image'
         self.fields['image'].widget.input_text = 'Upload the new Image'
 
-    is_sold = forms.BooleanField(required=False,widget=forms.CheckboxInput())
+    
        
