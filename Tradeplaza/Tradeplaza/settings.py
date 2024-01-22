54b73614c73a4dc965f6c3ef3e706623010a4963
Raaxxy <rahularyan0197@gmail.com>
2024-01-22 05:50:47 +0530

    update MEDIA_URL

--- Tradeplaza/Tradeplaza/settings.py
+++ Tradeplaza/Tradeplaza/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+MEDIA_URL = f'https://tradeplaza-bkt-01.s3.amazonaws.com/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
